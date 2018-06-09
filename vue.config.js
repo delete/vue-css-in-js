187ff3239f26dc81c2b09864d1915593d0f8173b
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2018-06-09 12:18:58 -0300

    test new configs

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,31 @@
 module.exports = {
-    lintOnSave: true,
-    configureWebpack: {
-        resolve: {
-            // Add `.ts` and `.tsx` as a resolvable extension.
-            extensions: [".ts", ".tsx", ".js"],
-            symlinks: true,
-            alias: {
-                '@': '/home/delete/Desenvolvimento/github_projects/vue-css-in-js/src',
-                vue$: 'vue/dist/vue.runtime.esm.js'
-            },
-        },
-        module: {
-            rules: [
-                // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-                {
-                    test: /\.ts|\.tsx$/,
-                    loader: ['babel-loader', 'awesome-typescript-loader'],
-                }
-            ]
+  configureWebpack: {
+    resolve: {
+      // Add `.ts` and `.tsx` as a resolvable extension.
+      extensions: [".ts", ".tsx", ".js"],
+      symlinks: true,
+    },
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        {
+          test: /\.ts|\.tsx$/,
+          loader: [
+            'babel-loader',
+            {
+              loader: 'awesome-typescript-loader',
+              options: {
+                useBabel: true,
+                useCache: true,
+                appendTsxSuffixTo: [
+                  /\.vue$/
+                ]
+              }
+            }
+          ],
         }
+      ]
     }
+  }
 }
